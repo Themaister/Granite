4b16c98408cde03866adbd02d523c6a5b486754c
Hans-Kristian Arntzen <maister@archlinux.us>
2017-03-29 18:41:14 +0200

    Herpy derp :)

--- event/ecs.hpp
+++ event/ecs.hpp
@@ -3,6 +3,8 @@
 #include <tuple>
 #include <vector>
 #include <memory>
+#include <unordered_set>
+#include <unordered_map>
 #include "object_pool.hpp"
 
 namespace Granite
@@ -46,25 +48,54 @@ private:
 	std::vector<Entity *> entities;
 };
 
+struct ComponentBase
+{
+
+};
+
 class ComponentAllocatorBase
 {
 public:
 	virtual ~ComponentAllocatorBase() = default;
+	virtual void free_component(ComponentBase *component) = 0;
 };
 
 template <typename T>
-class ComponentAllocator : public ComponentAllocatorBase
+struct ComponentAllocator : public ComponentAllocatorBase
 {
-public:
-private:
 	Util::ObjectPool<T> pool;
+
+	void free_component(ComponentBase *component) override
+	{
+		pool.free(static_cast<T *>(component));
+	}
 };
+
+struct Entity
+{
+	std::unordered_map<uint32_t, ComponentBase *> components;
+};
+
+using EntityHandle = std::shared_ptr<Entity>;
 
 class EntityPool
 {
 public:
+	EntityHandle create_entity();
+
+	template <typename T>
+	T *allocate_component()
+	{
+		uint32_t id = ComponentIDMapping::get_id<T>();
+		return nullptr;
+	}
+
+	void free_component(uint32_t id, ComponentBase *component);
+
 private:
+	Util::ObjectPool<Entity> entities;
 	std::unordered_map<uint32_t, std::unique_ptr<EntityGroupBase>> groups;
 	std::unordered_map<uint32_t, std::unique_ptr<ComponentAllocatorBase>> components;
+	std::unordered_map<uint32_t, std::unordered_set<uint32_t>> component_to_groups;
 };
 }

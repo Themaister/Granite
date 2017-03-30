eb2ce1d77b64e88c21de5b579c128a2c71cf5055
Hans-Kristian Arntzen <maister@archlinux.us>
2017-03-30 17:45:05 +0200

    Plug further.

--- event/ecs.hpp
+++ event/ecs.hpp
@@ -33,24 +33,53 @@ private:
 	static uint32_t group_ids;
 };
 
+struct ComponentBase
+{
+
+};
+
 class EntityGroupBase
 {
 public:
 	virtual ~EntityGroupBase() = default;
+	virtual void add_entity(const Entity &entity) = 0;
+	virtual void remove_component(uint32_t id, ComponentBase *component) = 0;
 };
 
 template <typename... Ts>
 class EntityGroup : public EntityGroupBase
 {
 public:
+	EntityGroup()
+	{
+		set_ids<Ts...>(group_ids);
+	}
+
+	void add_entity(const Entity &entity) override final
+	{
+	}
+
+	void remove_component(uint32_t id, ComponentBase *component) override final
+	{
+	}
+
 private:
 	std::vector<std::tuple<Ts *...>> groups;
 	std::vector<Entity *> entities;
-};
+	uint32_t group_ids[sizeof...(Ts)];
 
-struct ComponentBase
-{
+	template <typename U, typename... Us>
+	void set_ids(uint32_t *ids)
+	{
+		*ids = ComponentIDMapping::get_id<U>();
+		set_ids<Us...>(ids + 1);
+	}
 
+	template <typename U>
+	void set_ids(uint32_t *ids)
+	{
+		*ids = ComponentIDMapping::get_id<U>();
+	}
 };
 
 class ComponentAllocatorBase
@@ -65,7 +94,7 @@ struct ComponentAllocator : public ComponentAllocatorBase
 {
 	Util::ObjectPool<T> pool;
 
-	void free_component(ComponentBase *component) override
+	void free_component(ComponentBase *component) override final
 	{
 		pool.free(static_cast<T *>(component));
 	}
@@ -81,16 +110,40 @@ using EntityHandle = std::shared_ptr<Entity>;
 class EntityPool
 {
 public:
-	EntityHandle create_entity();
-
-	template <typename T>
-	T *allocate_component()
+	EntityHandle create_entity()
 	{
-		uint32_t id = ComponentIDMapping::get_id<T>();
-		return nullptr;
+		return std::make_shared<Entity>();
 	}
 
-	void free_component(uint32_t id, ComponentBase *component);
+	template <typename T, typename... Ts>
+	void allocate_component(Entity &entity, Ts&&... ts)
+	{
+		uint32_t id = ComponentIDMapping::get_id<T>();
+		auto itr = components.find(id);
+		if (itr == end(components))
+			itr = components.insert(make_pair(id, std::unique_ptr<ComponentAllocatorBase>(new ComponentAllocator<T>)));
+
+		auto *allocator = static_cast<ComponentAllocator<T> *>(itr->second.get());
+		auto &comp = entity.components[id];
+		if (comp)
+		{
+			allocator->free_component(comp);
+			comp = allocator->pool.allocate(std::forward<Ts>(ts)...);
+		}
+		else
+		{
+			comp = allocator->pool.allocate(std::forward<Ts>(ts)...);
+			for (auto &group : component_to_groups[id])
+				groups[group]->add_entity(entity);
+		}
+	}
+
+	void free_component(uint32_t id, ComponentBase *component)
+	{
+		components[id]->free_component(component);
+		for (auto &group : component_to_groups[id])
+			groups[group]->remove_component(id, component);
+	}
 
 private:
 	Util::ObjectPool<Entity> entities;

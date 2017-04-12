fea6df2928663bb19ebea84b77d98b15d80d060c
Hans-Kristian Arntzen <maister@archlinux.us>
2017-04-12 17:19:54 +0200

    Flesh out camera stuffs.

--- renderer/camera.cpp
+++ renderer/camera.cpp
@@ -2,5 +2,47 @@
 
 namespace Granite
 {
+void Camera::set_depth_range(float znear, float zfar)
+{
+	this->znear = znear;
+	this->zfar = zfar;
+}
 
+void Camera::set_aspect(float aspect)
+{
+	this->aspect = aspect;
+}
+
+void Camera::set_fovy(float fovy)
+{
+	this->fovy = fovy;
+}
+
+mat4 Camera::get_view() const
+{
+	return glm::translate(-position) * mat4_cast(rotation);
+}
+
+mat4 Camera::get_projection() const
+{
+	return glm::perspective(fovy, aspect, znear, zfar);
+}
+
+vec3 Camera::get_front() const
+{
+	auto d = get_view()[2].xyz();
+	return -d;
+}
+
+vec3 Camera::get_right() const
+{
+	auto d = get_view()[0].xyz();
+	return d;
+}
+
+vec3 Camera::get_up() const
+{
+	auto d = get_view()[1].xyz();
+	return d;
+}
 }
--- renderer/camera.hpp
+++ renderer/camera.hpp
@@ -10,6 +10,24 @@ public:
 	mat4 get_projection() const;
 	mat4 get_view() const;
 
+	void set_depth_range(float znear, float zfar);
+	void set_fovy(float fovy);
+	void set_aspect(float aspect);
+
+	vec3 get_front() const;
+	vec3 get_right() const;
+	vec3 get_up() const;
+
+	void set_position(const vec3 &pos);
+	void set_rotation(const quat &rot);
+	void look_at(const vec3 &eye, const vec3 &at, const vec3 &up);
+
 private:
+	vec3 position;
+	quat rotation;
+	float fovy;
+	float aspect;
+	float znear;
+	float zfar;
 };
 }

ddcacd216b06708964715d9c3db752a8f213ed7c
Hans-Kristian Arntzen <maister@archlinux.us>
2017-04-14 21:55:50 +0200

    Fiddle with GLM includes.

--- math/math.hpp
+++ math/math.hpp
@@ -3,7 +3,10 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #define GLM_FORCE_SWIZZLE
 #define GLM_ENABLE_EXPERIMENTAL
-#include "glm/glm.hpp"
+#include "glm/vec2.hpp"
+#include "glm/vec3.hpp"
+#include "glm/vec4.hpp"
+#include "glm/mat4x4.hpp"
 #include "glm/gtc/quaternion.hpp"
 #include "glm/gtx/transform.hpp"
 

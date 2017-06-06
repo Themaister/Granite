b05370cca7afe987bfdc1d4d905f82281343d352
Hans-Kristian Arntzen <maister@archlinux.us>
2017-06-06 12:33:21 +0200

    Enable immersive mode.

--- app/src/main/java/net/themaister/granite/GraniteActivity.java
+++ app/src/main/java/net/themaister/granite/GraniteActivity.java
@@ -1,9 +1,11 @@
 package net.themaister.granite;
 
-import android.hardware.SensorManager;
+import android.app.ActionBar;
+import android.os.Bundle;
 import android.view.Display;
-import android.view.SurfaceHolder;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class GraniteActivity extends android.app.NativeActivity
 {
@@ -16,6 +18,33 @@ public class GraniteActivity extends android.app.NativeActivity
                 finish();
             }
         });
+    }
+
+    private void setImmersiveMode()
+    {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
+    }
+
+    @Override
+    protected void onCreate(Bundle savedState)
+    {
+        super.onCreate(savedState);
+        setImmersiveMode();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus)
+    {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus)
+            setImmersiveMode();
     }
 
     public int getDisplayRotation()

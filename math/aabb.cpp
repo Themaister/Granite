6b78df5507ad44f18ba29a0252b1d7de7bfd8c07
Hans-Kristian Arntzen <maister@archlinux.us>
2017-06-26 19:57:54 +0200

    Minor optimizations.

--- math/aabb.cpp
+++ math/aabb.cpp
@@ -19,14 +19,6 @@ AABB AABB::transform(const mat4 &m) const
 	return AABB(m0, m1);
 }
 
-vec3 AABB::get_corner(unsigned i) const
-{
-	float x = i & 1 ? maximum.x : minimum.x;
-	float y = i & 2 ? maximum.y : minimum.y;
-	float z = i & 4 ? maximum.z : minimum.z;
-	return vec3(x, y, z);
-}
-
 vec3 AABB::get_coord(float dx, float dy, float dz) const
 {
 	return mix(minimum, maximum, vec3(dx, dy, dz));
@@ -38,13 +30,4 @@ void AABB::expand(const AABB &aabb)
 	maximum = max(maximum, aabb.maximum);
 }
 
-vec3 AABB::get_center() const
-{
-	return minimum + (maximum - minimum) * vec3(0.5f);
-}
-
-float AABB::get_radius() const
-{
-	return 0.5f * distance(minimum, maximum);
-}
 }
--- math/aabb.hpp
+++ math/aabb.hpp
@@ -14,10 +14,7 @@ public:
 
 	AABB() = default;
 
-	vec3 get_corner(unsigned i) const;
 	vec3 get_coord(float dx, float dy, float dz) const;
-	vec3 get_center() const;
-	float get_radius() const;
 	AABB transform(const mat4 &m) const;
 
 	void expand(const AABB &aabb);
@@ -30,6 +27,24 @@ public:
 	vec3 get_maximum() const
 	{
 		return maximum;
+	}
+
+	vec3 get_corner(unsigned i) const
+	{
+		float x = i & 1 ? maximum.x : minimum.x;
+		float y = i & 2 ? maximum.y : minimum.y;
+		float z = i & 4 ? maximum.z : minimum.z;
+		return vec3(x, y, z);
+	}
+
+	vec3 get_center() const
+	{
+		return minimum + (maximum - minimum) * vec3(0.5f);
+	}
+
+	float get_radius() const
+	{
+		return 0.5f * distance(minimum, maximum);
 	}
 
 private:

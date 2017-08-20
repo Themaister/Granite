49a26dca58585fdc9e6e68894fd1ca2a14acbe11
Hans-Kristian Arntzen <maister@archlinux.us>
2017-08-20 18:48:53 +0200

    Add some cubic spline interpolation.

---
+++ math/interpolation.cpp
@@ -0,0 +1,45 @@
+/* Copyright (c) 2017 Hans-Kristian Arntzen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "interpolation.hpp"
+
+namespace Granite
+{
+float catmull_rom_spline(float c0, float c1, float c2, float c3, float phase)
+{
+	float phase2 = phase * phase;
+	float phase3 = phase2 * phase;
+	return c1 +
+	       (c2 - c0) * 0.5f * phase +
+	       (c0 - (2.5f * c1) + (2.0f * c2) - (0.5f * c3)) * phase2 +
+	       ((-0.5f * c0) + (1.5f * c1) - (1.5f * c2) - (0.5f * c3)) * phase3;
+}
+
+// Computes the analytic derivative of the spline dFd(phase).
+float catmull_rom_spline_gradient(float c0, float c1, float c2, float c3, float phase)
+{
+	float phase2 = phase * phase;
+	return (c2 - c0) * 0.5f +
+	       (c0 - (2.5f * c1) + (2.0f * c2) - (0.5f * c3)) * 2.0f * phase +
+	       ((-0.5f * c0) + (1.5f * c1) - (1.5f * c2) - (0.5f * c3)) * 3.0f * phase2;
+}
+}

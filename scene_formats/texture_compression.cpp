e47f750f0d06abd12b684b29583e5e69b4863c8e
Hans-Kristian Arntzen <maister@archlinux.us>
2017-10-23 14:33:22 +0200

    Basic astc-encoder integration.

--- scene_formats/texture_compression.cpp
+++ scene_formats/texture_compression.cpp
@@ -29,6 +29,11 @@
 #include "ispc_texcomp/ispc_texcomp.h"
 #endif
 
+#ifdef HAVE_ASTC_ENCODER
+#undef IGNORE
+#include "astc_codec_internals.h"
+#endif
+
 using namespace std;
 
 namespace Granite
@@ -57,17 +62,25 @@ gli::format string_to_format(const string &s)
 		return gli::FORMAT_RGBA_ASTC_4X4_SRGB_BLOCK16;
 	else if (s == "astc_4x4_unorm")
 		return gli::FORMAT_RGBA_ASTC_4X4_UNORM_BLOCK16;
+	else if (s == "astc_4x4")
+		return gli::FORMAT_RGBA_ASTC_4X4_UNORM_BLOCK16;
 	else if (s == "astc_5x5_srgb")
 		return gli::FORMAT_RGBA_ASTC_5X5_SRGB_BLOCK16;
 	else if (s == "astc_5x5_unorm")
+		return gli::FORMAT_RGBA_ASTC_5X5_UNORM_BLOCK16;
+	else if (s == "astc_5x5")
 		return gli::FORMAT_RGBA_ASTC_5X5_UNORM_BLOCK16;
 	else if (s == "astc_6x6_srgb")
 		return gli::FORMAT_RGBA_ASTC_6X6_SRGB_BLOCK16;
 	else if (s == "astc_6x6_unorm")
 		return gli::FORMAT_RGBA_ASTC_6X6_UNORM_BLOCK16;
+	else if (s == "astc_6x6")
+		return gli::FORMAT_RGBA_ASTC_6X6_UNORM_BLOCK16;
 	else if (s == "astc_8x8_srgb")
 		return gli::FORMAT_RGBA_ASTC_8X8_SRGB_BLOCK16;
 	else if (s == "astc_8x8_unorm")
+		return gli::FORMAT_RGBA_ASTC_8X8_UNORM_BLOCK16;
+	else if (s == "astc_8x8")
 		return gli::FORMAT_RGBA_ASTC_8X8_UNORM_BLOCK16;
 	else
 	{
@@ -106,6 +119,154 @@ static unsigned format_to_stride(gli::format format)
 }
 #endif
 
+#ifdef HAVE_ASTC_ENCODER
+struct FirstASTC
+{
+	FirstASTC()
+	{
+		prepare_angular_tables();
+		build_quantization_mode_table();
+	}
+};
+
+static void compress_image_astc(uint8_t *output, const uint8_t *input, unsigned width, unsigned height,
+                                unsigned block_size_x, unsigned block_size_y, unsigned quality, bool hdr)
+{
+	static FirstASTC first_astc;
+
+	error_weighting_params ewp = {};
+	ewp.rgb_power = 1.0f;
+	ewp.alpha_power = 1.0f;
+	ewp.rgb_base_weight = 1.0f;
+	ewp.alpha_base_weight = 1.0f;
+
+	ewp.rgba_weights[0] = 1.0f;
+	ewp.rgba_weights[1] = 1.0f;
+	ewp.rgba_weights[2] = 1.0f;
+	ewp.rgba_weights[3] = 1.0f;
+
+	float log10_texels = log10(float(block_size_x * block_size_y));
+	float dblimit = std::max(95.0f - 35.0f * log10_texels, 70.0f - 19.0f * log10_texels);
+	swizzlepattern swizzle = { 0, 1, 2, 3 };
+
+	if (quality >= 3)
+	{
+		ewp.max_refinement_iters = 2;
+		ewp.block_mode_cutoff = 75.0f / 100.0f;
+		ewp.partition_1_to_2_limit = 1.2f;
+		ewp.lowest_correlation_cutoff = 0.75f;
+		ewp.partition_search_limit = 25;
+		dblimit = std::max(95.0f - 35.0f * log10_texels, 70.0f - 19.0f * log10_texels);
+	}
+	else if (quality == 2)
+	{
+		ewp.max_refinement_iters = 1;
+		ewp.block_mode_cutoff = 50.0f / 100.0f;
+		ewp.partition_1_to_2_limit = 1.0f;
+		ewp.lowest_correlation_cutoff = 0.5f;
+		ewp.partition_search_limit = 4;
+		dblimit = std::max(85.0f - 35.0f * log10_texels, 63.0f - 19.0f * log10_texels);
+	}
+	else if (quality <= 1)
+	{
+		ewp.max_refinement_iters = 1;
+		ewp.block_mode_cutoff = 25.0f / 100.0f;
+		ewp.partition_1_to_2_limit = 1.0f;
+		ewp.lowest_correlation_cutoff = 0.5f;
+		ewp.partition_search_limit = 2;
+		dblimit = std::max(70.0f - 35.0f * log10_texels, 53.0f - 19.0f * log10_texels);
+	}
+
+	if (hdr)
+	{
+		ewp.mean_stdev_radius = 0.0f;
+		ewp.rgb_power = 0.75f;
+		ewp.rgb_base_weight = 0.0f;
+		ewp.rgb_mean_weight = 1.0f;
+		ewp.alpha_power = 0.75f;
+		ewp.alpha_base_weight = 0.0f;
+		ewp.alpha_mean_weight = 1.0f;
+		ewp.partition_search_limit = PARTITION_COUNT;
+		ewp.texel_avg_error_limit = 0.0f;
+		rgb_force_use_of_hdr = 1;
+		alpha_force_use_of_hdr = 1;
+	}
+	else
+	{
+		rgb_force_use_of_hdr = 0;
+		alpha_force_use_of_hdr = 0;
+		ewp.texel_avg_error_limit = pow(0.1f, dblimit * 0.1f) * 65535.0f * 65535.0f;
+	}
+
+	float max_color_component_weight = std::max(std::max(ewp.rgba_weights[0], ewp.rgba_weights[1]),
+	                                            std::max(ewp.rgba_weights[2], ewp.rgba_weights[3]));
+	for (unsigned i = 0; i < 4; i++)
+		ewp.rgba_weights[i] = std::max(ewp.rgba_weights[i], max_color_component_weight / 1000.0f);
+	expand_block_artifact_suppression(block_size_x, block_size_y, 1, &ewp);
+
+	imageblock pb = {};
+	astc_codec_image astc_image = {};
+	astc_image.xsize = width;
+	astc_image.ysize = height;
+	astc_image.zsize = 1;
+	astc_image.padding = std::max(ewp.mean_stdev_radius, ewp.alpha_radius);
+
+	int exsize = astc_image.xsize + astc_image.padding * 2;
+	int eysize = astc_image.ysize + astc_image.padding * 2;
+	int estride = hdr ? 8 : 4;
+	vector<uint8_t *> rows(eysize);
+
+	vector<uint8_t> buffer(exsize * eysize * estride);
+	rows[0] = buffer.data();
+	for (int y = 1; y < eysize; y++)
+		rows[y] = rows[0] + y * exsize * estride;
+
+	for (int y = 0; y < eysize; y++)
+	{
+		for (int x = 0; x < exsize; x++)
+		{
+			int dst_offset = x + y * exsize;
+			int src_offset = clamp(x - astc_image.padding, 0, astc_image.xsize - 1) +
+			                 clamp(y - astc_image.padding, 0, astc_image.ysize - 1) * astc_image.xsize;
+			memcpy(&buffer[dst_offset * estride], &input[src_offset * estride], estride);
+		}
+	}
+
+	auto *ptr16 = reinterpret_cast<uint16_t **>(rows.data());
+	auto *ptr8 = rows.data();
+	// Triple-pointer wat.
+	if (hdr)
+		astc_image.imagedata16 = &ptr16;
+	else
+		astc_image.imagedata8 = &ptr8;
+
+	if (astc_image.padding > 0 || ewp.rgb_mean_weight != 0.0f || ewp.rgb_stdev_weight != 0.0f ||
+	    ewp.alpha_mean_weight != 0.0f || ewp.alpha_stdev_weight != 0.0f)
+	{
+		compute_averages_and_variances(&astc_image, ewp.rgb_power, ewp.alpha_power,
+		                               ewp.mean_stdev_radius, ewp.alpha_radius, swizzle);
+	}
+
+	int blocks_x = (width + block_size_x - 1) / block_size_x;
+	int blocks_y = (height + block_size_y - 1) / block_size_y;
+
+	symbolic_compressed_block scb;
+	physical_compressed_block pcb;
+
+	for (int y = 0; y < blocks_y; y++)
+	{
+		for (int x = 0; x < blocks_x; x++)
+		{
+			fetch_imageblock(&astc_image, &pb, block_size_x, block_size_y, 1, x * block_size_x, y * block_size_y, 0, swizzle);
+			compress_symbolic_block(&astc_image, hdr ? DECODE_HDR : DECODE_LDR,
+			                        block_size_x, block_size_y, 1, &ewp, &pb, &scb);
+			pcb = symbolic_to_physical(block_size_x, block_size_y, 1, &scb);
+			memcpy(output + 16 * (y * blocks_x + x), &pcb, sizeof(pcb));
+		}
+	}
+}
+#endif
+
 bool compress_texture(const CompressorArguments &args, const gli::texture &input)
 {
 	gli::texture output(input.target(), args.format, input.extent(), input.layers(), input.faces(), input.levels());
@@ -116,7 +277,9 @@ bool compress_texture(const CompressorArguments &args, const gli::texture &input
 	astc_enc_settings astc;
 #endif
 
-#ifdef HAVE_ISPC
+	bool use_astc_encoder = false;
+	bool use_hdr = false;
+
 	unsigned block_size_x = 1;
 	unsigned block_size_y = 1;
 
@@ -129,6 +292,7 @@ bool compress_texture(const CompressorArguments &args, const gli::texture &input
 			return false;
 		}
 
+#ifdef HAVE_ISPC
 		if (args.alpha)
 		{
 			if (args.quality <= 3)
@@ -138,10 +302,31 @@ bool compress_texture(const CompressorArguments &args, const gli::texture &input
 		}
 		else
 			GetProfile_astc_fast(&astc, x, y);
+#elif defined(HAVE_ASTC_ENCODER)
+		use_astc_encoder = true;
+#endif
 
 		return true;
 	};
+
+	const auto handle_astc_hdr_format = [&](unsigned x, unsigned y) -> bool {
+#ifdef HAVE_ASTC_ENCODER
+		if (input.format() != gli::FORMAT_RGBA16_SFLOAT_PACK16)
+		{
+			LOGE("Input format to ASTC HDR must be RGBA16F.\n");
+			return false;
+		}
+		use_astc_encoder = true;
+		use_hdr = true;
+		block_size_x = x;
+		block_size_y = y;
+		return true;
+#else
+		(void)x;
+		(void)y;
+		return false;
 #endif
+	};
 
 	switch (args.format)
 	{
@@ -248,31 +433,51 @@ bool compress_texture(const CompressorArguments &args, const gli::texture &input
 			return false;
 		}
 		break;
+#endif
 
 	case gli::FORMAT_RGBA_ASTC_4X4_SRGB_BLOCK16:
 	case gli::FORMAT_RGBA_ASTC_4X4_UNORM_BLOCK16:
-		if (!handle_astc_ldr_format(4, 4))
+		if (input.format() == gli::FORMAT_RGBA16_SFLOAT_PACK16)
+		{
+			if (!handle_astc_hdr_format(4, 4))
+				return false;
+		}
+		else if (!handle_astc_ldr_format(4, 4))
 			return false;
 		break;
 
 	case gli::FORMAT_RGBA_ASTC_5X5_SRGB_BLOCK16:
 	case gli::FORMAT_RGBA_ASTC_5X5_UNORM_BLOCK16:
-		if (!handle_astc_ldr_format(5, 5))
+		if (input.format() == gli::FORMAT_RGBA16_SFLOAT_PACK16)
+		{
+			if (!handle_astc_hdr_format(5, 5))
+				return false;
+		}
+		else if (!handle_astc_ldr_format(5, 5))
 			return false;
 		break;
 
 	case gli::FORMAT_RGBA_ASTC_6X6_SRGB_BLOCK16:
 	case gli::FORMAT_RGBA_ASTC_6X6_UNORM_BLOCK16:
-		if (!handle_astc_ldr_format(6, 6))
+		if (input.format() == gli::FORMAT_RGBA16_SFLOAT_PACK16)
+		{
+			if (!handle_astc_hdr_format(6, 6))
+				return false;
+		}
+		else if (!handle_astc_ldr_format(6, 6))
 			return false;
 		break;
 
 	case gli::FORMAT_RGBA_ASTC_8X8_SRGB_BLOCK16:
 	case gli::FORMAT_RGBA_ASTC_8X8_UNORM_BLOCK16:
-		if (!handle_astc_ldr_format(8, 8))
+		if (input.format() == gli::FORMAT_RGBA16_SFLOAT_PACK16)
+		{
+			if (!handle_astc_hdr_format(8, 8))
+				return false;
+		}
+		else if (!handle_astc_ldr_format(8, 8))
 			return false;
 		break;
-#endif
 
 	case gli::FORMAT_RGBA8_UNORM_PACK8:
 	case gli::FORMAT_RGBA8_SRGB_PACK8:
@@ -283,7 +488,6 @@ bool compress_texture(const CompressorArguments &args, const gli::texture &input
 		return false;
 	}
 
-#ifdef HAVE_ISPC
 	vector<uint8_t> padded_buffer;
 	for (unsigned layer = 0; layer < input.layers(); layer++)
 	{
@@ -291,6 +495,7 @@ bool compress_texture(const CompressorArguments &args, const gli::texture &input
 		{
 			for (unsigned level = 0; level < input.levels(); level++)
 			{
+#ifdef HAVE_ISPC
 				rgba_surface surface = {};
 				surface.ptr = const_cast<uint8_t *>(static_cast<const uint8_t *>(input.data(layer, face, level)));
 				surface.width = input.extent(level).x;
@@ -313,9 +518,11 @@ bool compress_texture(const CompressorArguments &args, const gli::texture &input
 					padded_surface = surface;
 					padded_surface.stride = padded_surface.width * format_to_stride(args.format);
 				}
+#endif
 
 				switch (args.format)
 				{
+#ifdef HAVE_ISPC
 				case gli::FORMAT_RGB_BP_UFLOAT_BLOCK16:
 					CompressBlocksBC6H(&padded_surface, static_cast<uint8_t *>(output.data(layer, face, level)), &bc6);
 					break;
@@ -334,6 +541,7 @@ bool compress_texture(const CompressorArguments &args, const gli::texture &input
 				case gli::FORMAT_RGBA_DXT5_UNORM_BLOCK16:
 					CompressBlocksBC3(&padded_surface, static_cast<uint8_t *>(output.data(layer, face, level)));
 					break;
+#endif
 
 				case gli::FORMAT_RGBA_ASTC_4X4_SRGB_BLOCK16:
 				case gli::FORMAT_RGBA_ASTC_4X4_UNORM_BLOCK16:
@@ -343,7 +551,22 @@ bool compress_texture(const CompressorArguments &args, const gli::texture &input
 				case gli::FORMAT_RGBA_ASTC_6X6_UNORM_BLOCK16:
 				case gli::FORMAT_RGBA_ASTC_8X8_SRGB_BLOCK16:
 				case gli::FORMAT_RGBA_ASTC_8X8_UNORM_BLOCK16:
-					CompressBlocksASTC(&padded_surface, static_cast<uint8_t *>(output.data(layer, face, level)), &astc);
+#ifdef HAVE_ISPC
+					if (!use_astc_encoder)
+						CompressBlocksASTC(&padded_surface, static_cast<uint8_t *>(output.data(layer, face, level)), &astc);
+					else
+#endif
+					{
+#ifdef HAVE_ASTC_ENCODER
+						compress_image_astc(static_cast<uint8_t *>(output.data(layer, face, level)),
+						                    static_cast<const uint8_t *>(input.data(layer, face, level)),
+						                    input.extent(level).x,
+						                    input.extent(level).y,
+						                    block_size_x, block_size_y, args.quality, use_hdr);
+#else
+						return false;
+#endif
+					}
 					break;
 
 				default:
@@ -352,7 +575,6 @@ bool compress_texture(const CompressorArguments &args, const gli::texture &input
 			}
 		}
 	}
-#endif
 
 	if (!save_texture_to_file(args.output, output))
 	{

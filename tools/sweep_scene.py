ac142e96cc5b65f92043a1b5d7b378780ce3b4a5
Hans-Kristian Arntzen <maister@archlinux.us>
2017-11-14 10:27:31 +0100

    Add cluster stencil mode to sweep.

--- tools/sweep_scene.py
+++ tools/sweep_scene.py
@@ -51,6 +51,7 @@ def config_to_path(c):
     res += '_positionalshadow' if c['clusteredLightsShadows'] else ''
     res += ('_camera_' + str(c['cameraIndex'])) if ('cameraIndex' in c) else ''
     res += '_hdrbloom' if c['hdrBloom'] else '_ldr'
+    res += '_clusterstencil' if c['deferredClusteredStencilCulling'] else ''
     return res
 
 def main():
@@ -261,51 +262,56 @@ def main():
                     if prepass and renderer == 'deferred':
                         continue
                     for clustered in [False, True]:
-                        for hdr_bloom in [False, True]:
-                            for shadows in [False, True]:
-                                for pos_shadows in [False, True]:
-                                    if pos_shadows and renderer == 'forward' and (not clustered):
-                                        continue
-                                    c = {}
-                                    c['renderer'] = renderer
-                                    c['hdrBloom'] = hdr_bloom
-                                    c['msaa'] = msaa
-                                    c['clusteredLights'] = clustered
-                                    c['directionalLightShadows'] = shadows
-                                    c['forwardDepthPrepass'] = prepass
-                                    c['clusteredLightsShadows'] = pos_shadows
-                                    c['showUi'] = False
-                                    if args.gen_configs_camera_index is not None:
-                                        c['cameraIndex'] = args.gen_configs_camera_index
-                                    with open(config_file, 'w') as f:
-                                        json.dump(c, f)
+                        for stencil_culling in [False, True]:
+                            if stencil_culling and (renderer != 'deferred' or clustered):
+                                continue
+                            for hdr_bloom in [False, True]:
+                                for shadows in [False, True]:
+                                    for pos_shadows in [False, True]:
+                                        if pos_shadows and renderer == 'forward' and (not clustered):
+                                            continue
+                                        c = {}
+                                        c['renderer'] = renderer
+                                        c['hdrBloom'] = hdr_bloom
+                                        c['msaa'] = msaa
+                                        c['clusteredLights'] = clustered
+                                        c['directionalLightShadows'] = shadows
+                                        c['forwardDepthPrepass'] = prepass
+                                        c['clusteredLightsShadows'] = pos_shadows
+                                        c['showUi'] = False
+                                        c['deferredClusteredStencilCulling'] = stencil_culling
+                                        if args.gen_configs_camera_index is not None:
+                                            c['cameraIndex'] = args.gen_configs_camera_index
+                                        with open(config_file, 'w') as f:
+                                            json.dump(c, f)
 
-                                    if args.android_viewer_binary is not None:
-                                        sweep = base_sweep + ['--config', '/data/local/tmp/granite/config.json']
-                                        subprocess.check_call(['adb', 'push', config_file, '/data/local/tmp/granite/config.json'])
-                                        if args.png_result_dir:
-                                            sweep.append('--png-reference-path')
-                                            sweep.append('/data/local/tmp/granite/ref.png')
-                                    else:
-                                        sweep = base_sweep + ['--config', config_file]
-                                        if args.png_result_dir:
-                                            sweep.append('--png-reference-path')
-                                            sweep.append(os.path.join(args.png_result_dir, config_to_path(c)) + '.png')
+                                        if args.android_viewer_binary is not None:
+                                            sweep = base_sweep + ['--config', '/data/local/tmp/granite/config.json']
+                                            subprocess.check_call(['adb', 'push', config_file, '/data/local/tmp/granite/config.json'])
+                                            if args.png_result_dir:
+                                                sweep.append('--png-reference-path')
+                                                sweep.append('/data/local/tmp/granite/ref.png')
+                                        else:
+                                            sweep = base_sweep + ['--config', config_file]
+                                            if args.png_result_dir:
+                                                sweep.append('--png-reference-path')
+                                                sweep.append(os.path.join(args.png_result_dir, config_to_path(c)) + '.png')
 
-                                    avg, stddev, gpu, version = run_test(sweep, config_file, iterations, stat_file, args.android_viewer_binary is not None)
+                                        avg, stddev, gpu, version = run_test(sweep, config_file, iterations, stat_file, args.android_viewer_binary is not None)
 
-                                    if (args.android_viewer_binary  is not None) and (args.png_result_dir is not None):
-                                        subprocess.check_call(['adb', 'pull', '/data/local/tmp/granite/ref.png', os.path.join(args.png_result_dir, config_to_path(c)) + '.png'])
+                                        if (args.android_viewer_binary  is not None) and (args.png_result_dir is not None):
+                                            subprocess.check_call(['adb', 'pull', '/data/local/tmp/granite/ref.png', os.path.join(args.png_result_dir, config_to_path(c)) + '.png'])
 
-                                    config_name = {}
-                                    config_name['renderer'] = renderer
-                                    config_name['msaa'] = msaa
-                                    config_name['prepass'] = prepass
-                                    config_name['clustered'] = clustered
-                                    config_name['hdr_bloom'] = hdr_bloom
-                                    config_name['shadows'] = shadows
-                                    config_name['pos_shadows'] = pos_shadows
-                                    results.append((config_name, avg, stddev))
+                                        config_name = {}
+                                        config_name['renderer'] = renderer
+                                        config_name['msaa'] = msaa
+                                        config_name['prepass'] = prepass
+                                        config_name['clustered'] = clustered
+                                        config_name['hdr_bloom'] = hdr_bloom
+                                        config_name['shadows'] = shadows
+                                        config_name['pos_shadows'] = pos_shadows
+                                        config_name['stencil_culling'] = stencil_culling
+                                        results.append((config_name, avg, stddev))
 
     for res in results:
         print(res)
--- tools/sweep_stat_diff.py
+++ tools/sweep_stat_diff.py
@@ -11,7 +11,7 @@ def read_stat_file(path):
         parsed = json.loads(json_data)
         return parsed
 
-def find_run(stats, renderer, msaa, prepass, clustered, hdr_bloom, shadows, pos_shadows):
+def find_run(stats, renderer, msaa, prepass, clustered, stencil_culling, hdr_bloom, shadows, pos_shadows):
     for run in stats['runs']:
         config = run['config']
 
@@ -20,10 +20,11 @@ def find_run(stats, renderer, msaa, prepass, clustered, hdr_bloom, shadows, pos_
             if config['msaa'] == msaa:
                 if config['prepass'] == prepass:
                     if config['clustered'] == clustered:
-                        if config['hdr_bloom'] == hdr_bloom:
-                            if config['shadows'] == shadows:
-                                if config['pos_shadows'] == pos_shadows:
-                                    return run
+                        if config['stencil_culling'] == stencil_culling:
+                            if config['hdr_bloom'] == hdr_bloom:
+                                if config['shadows'] == shadows:
+                                    if config['pos_shadows'] == pos_shadows:
+                                        return run
     return None
 
 def main():
@@ -52,40 +53,44 @@ def main():
                 if prepass and renderer == 'deferred':
                     continue
                 for clustered in [False, True]:
-                    for hdr_bloom in [False, True]:
-                        for shadows in [False, True]:
-                            for pos_shadows in [False, True]:
-                                if pos_shadows and renderer == 'forward' and (not clustered):
-                                    continue
-                                type_str = ''
-                                type_str += 'F' if renderer == 'forward' else 'D'
-                                type_str += str(msaa)
-                                type_str += 'Z' if prepass else 'z'
-                                type_str += 'C' if clustered else 'c'
-                                type_str += 'H' if hdr_bloom else 'L'
-                                type_str += 'SS' if shadows else 'ss'
-                                type_str += 'PS' if pos_shadows else 'ps'
-                                result_string = '{:15}'.format(type_str)
+                    for stencil_culling in [False, True]:
+                        if stencil_culling and (renderer != 'deferred' or clustered):
+                            continue
+                        for hdr_bloom in [False, True]:
+                            for shadows in [False, True]:
+                                for pos_shadows in [False, True]:
+                                    if pos_shadows and renderer == 'forward' and (not clustered):
+                                        continue
+                                    type_str = ''
+                                    type_str += 'F' if renderer == 'forward' else 'D'
+                                    type_str += str(msaa)
+                                    type_str += 'Z' if prepass else 'z'
+                                    type_str += 'C' if clustered else 'c'
+                                    type_str += 'S' if stencil_culling else 's'
+                                    type_str += 'H' if hdr_bloom else 'L'
+                                    type_str += 'SS' if shadows else 'ss'
+                                    type_str += 'PS' if pos_shadows else 'ps'
+                                    result_string = '{:15}'.format(type_str)
 
-                                first = True
-                                reference_time = 0.0
+                                    first = True
+                                    reference_time = 0.0
 
-                                for stat in stats:
-                                    run = find_run(stat, renderer, msaa, prepass, clustered, hdr_bloom, shadows, pos_shadows)
-                                    if run is not None:
-                                        if first:
-                                            reference_time = run['avg']
+                                    for stat in stats:
+                                        run = find_run(stat, renderer, msaa, prepass, clustered, stencil_culling, hdr_bloom, shadows, pos_shadows)
+                                        if run is not None:
+                                            if first:
+                                                reference_time = run['avg']
 
-                                        if not first:
-                                            result_string += '{:>25}'.format('{:.3f}'.format(run['avg'] / 1000.0) + ' ms ' + '({:6.2f} %)'.format(((run['avg'] - reference_time) / reference_time) * 100.0))
+                                            if not first:
+                                                result_string += '{:>25}'.format('{:.3f}'.format(run['avg'] / 1000.0) + ' ms ' + '({:6.2f} %)'.format(((run['avg'] - reference_time) / reference_time) * 100.0))
+                                            else:
+                                                result_string += '{:>25}'.format('{:.3f}'.format(run['avg'] / 1000.0) + ' ms')
+
+                                            first = False
                                         else:
-                                            result_string += '{:>25}'.format('{:.3f}'.format(run['avg'] / 1000.0) + ' ms')
+                                            result_string += '{:>25}'.format('N/A')
 
-                                        first = False
-                                    else:
-                                        result_string += '{:>25}'.format('N/A')
-
-                                print(result_string)
+                                    print(result_string)
 
 
 if __name__ == '__main__':

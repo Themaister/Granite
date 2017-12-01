3b77a281f6d96a542d78a02a7586e1e2d8418b92
Hans-Kristian Arntzen <maister@archlinux.us>
2017-12-01 16:35:39 +0100

    Add PCF 5x5.

--- assets/shaders/lights/pcf.h
+++ assets/shaders/lights/pcf.h
@@ -1,9 +1,47 @@
 #ifndef PCF_H_
 #define PCF_H_
 
-#define PCF_3x3
+#define PCF_5x5
 
-#ifdef PCF_3x3
+#if defined(PCF_5x5)
+#define SAMPLE_PCF(tex, uv, x, y) textureLodOffset(tex, uv, 0.0, ivec2(x, y))
+#define SAMPLE_PCF_KERNEL(var, tex, uv) \
+{ \
+	vec3 clip_uv = (uv).xyz / (uv).w; \
+	const mediump float weight = 0.1177491; \
+	const mediump float w00 = weight * 1.000000; \
+	const mediump float w01 = weight * 0.707106; \
+	const mediump float w02 = weight * 0.250000; \
+	const mediump float w11 = weight * 0.5; \
+	const mediump float w12 = weight * 0.176776; \
+	const mediump float w22 = weight * 0.0625; \
+	var = w22 * SAMPLE_PCF(tex, clip_uv, -2, -2); \
+	var += w12 * SAMPLE_PCF(tex, clip_uv, -1, -2); \
+	var += w02 * SAMPLE_PCF(tex, clip_uv, +0, -2); \
+	var += w12 * SAMPLE_PCF(tex, clip_uv, +1, -2); \
+	var += w22 * SAMPLE_PCF(tex, clip_uv, +2, -2); \
+	var += w12 * SAMPLE_PCF(tex, clip_uv, -2, -1); \
+	var += w11 * SAMPLE_PCF(tex, clip_uv, -1, -1); \
+	var += w01 * SAMPLE_PCF(tex, clip_uv, +0, -1); \
+	var += w11 * SAMPLE_PCF(tex, clip_uv, +1, -1); \
+	var += w12 * SAMPLE_PCF(tex, clip_uv, +2, -1); \
+	var += w02 * SAMPLE_PCF(tex, clip_uv, -2, +0); \
+	var += w01 * SAMPLE_PCF(tex, clip_uv, -1, +0); \
+	var += w00 * SAMPLE_PCF(tex, clip_uv, +0, +0); \
+	var += w01 * SAMPLE_PCF(tex, clip_uv, +1, +0); \
+	var += w02 * SAMPLE_PCF(tex, clip_uv, +2, +0); \
+	var += w12 * SAMPLE_PCF(tex, clip_uv, -2, +1); \
+	var += w11 * SAMPLE_PCF(tex, clip_uv, -1, +1); \
+	var += w01 * SAMPLE_PCF(tex, clip_uv, +0, +1); \
+	var += w11 * SAMPLE_PCF(tex, clip_uv, +1, +1); \
+	var += w12 * SAMPLE_PCF(tex, clip_uv, +2, +1); \
+	var += w22 * SAMPLE_PCF(tex, clip_uv, -2, +2); \
+	var += w12 * SAMPLE_PCF(tex, clip_uv, -1, +2); \
+	var += w02 * SAMPLE_PCF(tex, clip_uv, +0, +2); \
+	var += w12 * SAMPLE_PCF(tex, clip_uv, +1, +2); \
+	var += w22 * SAMPLE_PCF(tex, clip_uv, +2, +2); \
+}
+#elif defined(PCF_3x3)
 #define SAMPLE_PCF(tex, uv, x, y) textureLodOffset(tex, uv, 0.0, ivec2(x, y))
 #define SAMPLE_PCF_KERNEL(var, tex, uv) \
 { \

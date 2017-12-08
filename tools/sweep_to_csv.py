ef883cd173fafac7ca5f28cc7e40a7c0f802645c
Hans-Kristian Arntzen <maister@archlinux.us>
2017-12-08 17:50:54 +0100

    Add shadow type to CSV.

--- tools/sweep_to_csv.py
+++ tools/sweep_to_csv.py
@@ -11,6 +11,12 @@ def read_stat_file(path):
         parsed = json.loads(json_data)
         return parsed
 
+def shadow_type_to_str(cfg):
+    if cfg['vsm']:
+        return 'VSM'
+    else:
+        return 'PCF' + str(cfg['pcf_width'])
+
 def main():
     parser = argparse.ArgumentParser(description = 'Converts a sweep file to CSV.')
     parser.add_argument('--stat',
@@ -26,10 +32,10 @@ def main():
 
     stats = read_stat_file(args.stat)
 
-    lines = [','.join(['Renderer', 'MSAA', 'Depth prepass', 'Clustered shading', 'Stencil culling', 'HDR bloom', 'Sun shadows', 'Positional shadows', 'Average time ' + stats['runs'][0]['gpu'], 'Stddev ' + stats['runs'][0]['gpu']]) + '\n']
+    lines = [','.join(['Renderer', 'MSAA', 'Depth prepass', 'Clustered shading', 'Stencil culling', 'HDR bloom', 'Sun shadows', 'Positional shadows', 'Shadow type', 'Average time ' + stats['runs'][0]['gpu'], 'Stddev ' + stats['runs'][0]['gpu']]) + '\n']
     for run in stats['runs']:
         config = run['config']
-        line = [config['renderer'], config['msaa'], config['prepass'], config['clustered'], config['stencil_culling'], config['hdr_bloom'], config['shadows'], config['pos_shadows'], run['avg'], run['stdev']]
+        line = [config['renderer'], config['msaa'], config['prepass'], config['clustered'], config['stencil_culling'], config['hdr_bloom'], config['shadows'], config['pos_shadows'], shadow_type_to_str(config), run['avg'], run['stdev']]
         line = [str(x) for x in line]
         lines.append(','.join(line) + '\n')
 

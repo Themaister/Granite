ab474ea43ce79c61dcc3ab85042a87c2497a3295
Hans-Kristian Arntzen <maister@archlinux.us>
2019-02-18 15:55:30 +0100

    Start adding a lock-less message queue.

---
+++ util/lock_free_message_queue.cpp
@@ -0,0 +1,101 @@
+/* Copyright (c) 2017-2019 Hans-Kristian Arntzen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "lock_free_message_queue.hpp"
+#include "util.hpp"
+#include "aligned_alloc.hpp"
+#include <stdlib.h>
+
+namespace Util
+{
+void MessageQueuePayloadDeleter::operator()(void *ptr)
+{
+	memalign_free(ptr);
+}
+
+LockFreeMessageQueue::LockFreeMessageQueue()
+{
+	for (unsigned i = 0; i < 8; i++)
+		payload_capacity[i] = 256u << i;
+	for (unsigned i = 0; i < 8; i++)
+		write_ring[i].reset((16u * 1024u) >> i);
+	read_ring.reset(32 * 1024);
+
+	// Pre-fill the rings.
+	for (unsigned i = 0; i < 8; i++)
+	{
+		unsigned count = 1024u >> i;
+		for (unsigned j = 0; j < count; j++)
+		{
+			MessageQueuePayload payload;
+			payload.set_payload_data(memalign_calloc(64, payload_capacity[i]), payload_capacity[i]);
+			recycle_payload(std::move(payload));
+		}
+	}
+}
+
+size_t LockFreeMessageQueue::available_read_messages() const
+{
+	return read_ring.read_avail();
+}
+
+MessageQueuePayload LockFreeMessageQueue::read_message()
+{
+	MessageQueuePayload payload;
+	read_ring.read_and_move(payload);
+	return payload;
+}
+
+void LockFreeMessageQueue::recycle_payload(MessageQueuePayload payload)
+{
+	for (unsigned i = 0; i < 3; i++)
+	{
+		if (payload.get_capacity() == payload_capacity[i])
+		{
+			if (!write_ring[i].write_and_move(std::move(payload)))
+				LOGE("Failed to recycle write payload. Increase sizes!\n");
+			return;
+		}
+	}
+}
+
+MessageQueuePayload LockFreeMessageQueue::allocate_write_payload(size_t size)
+{
+	MessageQueuePayload payload;
+	for (unsigned i = 0; i < 3; i++)
+	{
+		if (size <= payload_capacity[i])
+		{
+			if (!write_ring[i].read_and_move(payload))
+			{
+				LOGE("Failed to allocate write payload. Have to allocate a new one on the fly. Increase sizes!\n");
+				payload.set_payload_data(memalign_calloc(64, payload_capacity[i]), payload_capacity[i]);
+			}
+			return payload;
+		}
+	}
+
+	LOGE("Failed to allocate any payload. Making a temporary one. Increase max payload size!\n");
+	payload.set_payload_data(memalign_calloc(64, size), size);
+	return payload;
+}
+}
---
+++ util/lock_free_message_queue.hpp
@@ -0,0 +1,200 @@
+/* Copyright (c) 2017-2019 Hans-Kristian Arntzen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#pragma once
+
+#include <atomic>
+#include <vector>
+#include <utility>
+#include <stddef.h>
+#include <assert.h>
+#include <memory>
+
+namespace Util
+{
+// There can only be one concurrent reader, and one concurrent writer.
+// This is useful for lock-less messaging between two threads, e.g. a worker thread and master thread.
+template <typename T>
+class LockFreeRingBuffer
+{
+public:
+	LockFreeRingBuffer()
+	{
+		reset(1);
+		assert(read_count.is_lock_free());
+		assert(write_count.is_lock_free());
+	}
+
+	void reset(size_t count)
+	{
+		//assert((count & (count - 1)) == 0);
+		ring.resize(count);
+		read_count.store(0);
+		write_count.store(0);
+	}
+
+	size_t read_avail() const
+	{
+		return write_count.load(std::memory_order_acquire) -
+		read_count.load(std::memory_order_relaxed);
+	}
+
+	size_t write_avail() const
+	{
+		return ring.size() -
+		       (write_count.load(std::memory_order_relaxed) -
+		        read_count.load(std::memory_order_acquire));
+	}
+
+	bool write_and_move(T *values, size_t count)
+	{
+		size_t current_written = read_count.load(std::memory_order_relaxed);
+		size_t current_read = write_count.load(std::memory_order_acquire);
+		if (count >= ring.size() - (current_written - current_read))
+			return false;
+
+		size_t can_write_first = std::min(ring.size() - write_offset, count);
+		size_t can_write_second = count - can_write_first;
+		std::move(values, values + can_write_first, ring.data() + write_offset);
+
+		write_offset += can_write_first;
+		values += can_write_first;
+		if (write_offset >= ring.size())
+			write_offset -= ring.size();
+
+		std::move(values, values + can_write_second, ring.data());
+		write_offset += can_write_second;
+
+		// Need release ordering so the reads here can't be ordered after the store.
+		write_count.store(write_count.load(std::memory_order_relaxed) + count, std::memory_order_release);
+		return true;
+	}
+
+	bool read_and_move(T *values, size_t count)
+	{
+		size_t current_read = read_count.load(std::memory_order_relaxed);
+		size_t current_written = write_count.load(std::memory_order_acquire);
+		if (count >= current_written - current_read)
+			return false;
+
+		size_t can_read_first = std::min(ring.size() - read_offset, count);
+		size_t can_read_second = count - can_read_first;
+		std::move(ring.data() + read_offset, ring.data() + read_offset + can_read_first, values);
+
+		read_offset += can_read_first;
+		values += can_read_first;
+		if (read_offset >= ring.size())
+			read_offset -= ring.size();
+
+		std::move(ring.data(), ring.data() + can_read_second, values);
+		read_offset += can_read_second;
+
+		// Need release ordering so the reads here can't be ordered after the store.
+		read_count.store(read_count.load(std::memory_order_relaxed) + count, std::memory_order_release);
+		return true;
+	}
+
+	bool write_and_move(T value)
+	{
+		return write_and_move(&value, 1);
+	}
+
+	bool read_and_move(T &value)
+	{
+		return read_and_move(&value, 1);
+	}
+
+private:
+	std::atomic<size_t> read_count;
+	std::atomic<size_t> write_count;
+	size_t read_offset = 0;
+	size_t write_offset = 0;
+	std::vector<T> ring;
+};
+
+struct MessageQueuePayloadDeleter
+{
+	void operator()(void *ptr);
+};
+
+class MessageQueuePayload
+{
+public:
+	template <typename T>
+	T &as()
+	{
+		assert(payload);
+		return *static_cast<T *>(payload.get());
+	}
+
+	explicit operator bool() const
+	{
+		return bool(payload);
+	}
+
+	size_t get_size() const
+	{
+		return payload_size;
+	}
+
+	void set_size(size_t size)
+	{
+		assert(size <= payload_capacity);
+		payload_size = size;
+	}
+
+	void set_payload_data(void *ptr, size_t size)
+	{
+		payload.reset(ptr);
+		payload_capacity = size;
+	}
+
+	size_t get_capacity() const
+	{
+		return payload_capacity;
+	}
+
+private:
+	std::unique_ptr<void, MessageQueuePayloadDeleter> payload;
+	size_t payload_size = 0;
+	size_t payload_capacity = 0;
+};
+
+class LockFreeMessageQueue
+{
+public:
+	LockFreeMessageQueue();
+
+	MessageQueuePayload allocate_write_payload(size_t size);
+	void push_written_payload(MessageQueuePayload payload);
+
+	size_t available_read_messages() const;
+	MessageQueuePayload read_message();
+
+	void recycle_payload(MessageQueuePayload payload);
+
+private:
+	LockFreeRingBuffer<MessageQueuePayload> read_ring;
+	LockFreeRingBuffer<MessageQueuePayload> write_ring[8];
+	size_t payload_capacity[8] = {};
+};
+}

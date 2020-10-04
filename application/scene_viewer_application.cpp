80078fa44b93ab459817a7861adc41feacabf0de
Hans-Kristian Arntzen <maister@archlinux.us>
2020-10-04 15:21:14 +0200

    Fix crash when timeline is not used.

--- application/scene_viewer_application.cpp
+++ application/scene_viewer_application.cpp
@@ -1115,17 +1115,25 @@ void SceneViewerApplication::render_frame(double frame_time, double elapsed_time
 	auto *file = Global::thread_group()->get_timeline_trace_file();
 	TaskComposer composer(*Global::thread_group());
 
-	auto *e = file->begin_event("update-scene-enqueue");
+	Util::TimelineTraceFile::Event *e = nullptr;
+
+	if (file)
+		e = file->begin_event("update-scene-enqueue");
 	update_scene(composer, frame_time, elapsed_time);
-	file->end_event(e);
+	if (e)
+		file->end_event(e);
 
-	e = file->begin_event("render-scene-enqueue");
+	if (file)
+		e = file->begin_event("render-scene-enqueue");
 	render_scene(composer);
-	file->end_event(e);
+	if (e)
+		file->end_event(e);
 
-	e = file->begin_event("render-frame-wait");
+	if (file)
+		e = file->begin_event("render-frame-wait");
 	composer.get_outgoing_task()->wait();
-	file->end_event(e);
+	if (e)
+		file->end_event(e);
 }
 
 } // namespace Granite

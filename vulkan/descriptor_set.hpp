216a6cd9d79ce7a82e8082fd861f6e62e28e3cf4
Hans-Kristian Arntzen <maister@archlinux.us>
2021-03-28 13:29:18 +0200

    Bump descriptor limit to 32 per stage.
    Builtin set is starting to get crowded ...

--- vulkan/descriptor_set.hpp
+++ vulkan/descriptor_set.hpp
@@ -47,7 +47,7 @@ struct DescriptorSetLayout
 	uint32_t separate_image_mask = 0;
 	uint32_t fp_mask = 0;
 	uint32_t immutable_sampler_mask = 0;
-	uint64_t immutable_samplers = 0;
+	uint32_t immutable_samplers[(VULKAN_NUM_BINDINGS * 4) / 32] = {};
 	uint8_t array_size[VULKAN_NUM_BINDINGS] = {};
 	enum { UNSIZED_ARRAY = 0xff };
 };
@@ -62,12 +62,19 @@ static inline bool has_immutable_sampler(const DescriptorSetLayout &layout, unsi
 static inline StockSampler get_immutable_sampler(const DescriptorSetLayout &layout, unsigned binding)
 {
 	VK_ASSERT(has_immutable_sampler(layout, binding));
-	return static_cast<StockSampler>((layout.immutable_samplers >> (4 * binding)) & 0xf);
+
+	unsigned bit = 4 * binding;
+	unsigned word_offset = bit >> 5;
+	unsigned bit_offset = bit & 31;
+	return static_cast<StockSampler>((layout.immutable_samplers[word_offset] >> bit_offset) & 0xf);
 }
 
 static inline void set_immutable_sampler(DescriptorSetLayout &layout, unsigned binding, StockSampler sampler)
 {
-	layout.immutable_samplers |= uint64_t(sampler) << (4 * binding);
+	unsigned bit = 4 * binding;
+	unsigned word_offset = bit >> 5;
+	unsigned bit_offset = bit & 31;
+	layout.immutable_samplers[word_offset] |= uint32_t(sampler) << bit_offset;
 	layout.immutable_sampler_mask |= 1u << binding;
 }
 

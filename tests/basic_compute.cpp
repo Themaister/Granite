ef1b1467205dea05db90c8094f3dc77da87b1cc4
Hans-Kristian Arntzen <maister@archlinux.us>
2021-07-13 14:33:02 +0200

    Mat3 repro

--- tests/basic_compute.cpp
+++ tests/basic_compute.cpp
@@ -84,38 +84,55 @@ struct BasicComputeTest : Granite::Application, Granite::EventHandler
 		auto &device = get_wsi().get_device();
 		auto cmd = device.request_command_buffer();
 
-		cmd->barrier(VK_PIPELINE_STAGE_ALL_COMMANDS_BIT, VK_ACCESS_MEMORY_WRITE_BIT, VK_PIPELINE_STAGE_ALL_COMMANDS_BIT,
-		             VK_ACCESS_MEMORY_READ_BIT);
+		cmd->barrier(VK_PIPELINE_STAGE_ALL_COMMANDS_BIT, VK_ACCESS_MEMORY_WRITE_BIT,
+		             VK_PIPELINE_STAGE_ALL_COMMANDS_BIT, VK_ACCESS_MEMORY_READ_BIT);
 
-		uint32_t variants[64];
-		for (unsigned i = 0; i < 64; i++)
-			variants[i] = i & 3;
-		auto variant_buffer = create_ssbo(variants, sizeof(variants));
+		mat3 inputs[128];
+		for (unsigned i = 0; i < 9 * 128; i++)
+			*(&inputs[0][0][0] + i) = float(i) + 1.0f;
+		auto inputs_buffer = create_ssbo(inputs, sizeof(inputs));
+		auto outputs_buffer = create_ssbo(nullptr, sizeof(vec3) * 3);
 
-		auto work_list_buffer = create_ssbo(nullptr, 64 * 64 * sizeof(uint32_t));
-
-		uint32_t counts[64] = {};
-		auto work_list_count = create_ssbo(counts, sizeof(counts));
-
-		cmd->set_program("assets://shaders/compute_bucket_allocate.comp");
-		cmd->set_storage_buffer(0, 0, *variant_buffer);
-		cmd->set_storage_buffer(0, 1, *work_list_buffer);
-		cmd->set_storage_buffer(0, 2, *work_list_count);
+		cmd->set_program("assets://shaders/mat3_reduce.comp");
+		cmd->set_storage_buffer(0, 0, *inputs_buffer);
+		cmd->set_storage_buffer(0, 1, *outputs_buffer);
+		//*cmd->allocate_typed_constant_data<uvec2>(0, 2, 1) = uvec2(0, 2);
 		cmd->dispatch(1, 1, 1);
 		device.submit(cmd);
 
-		uint32_t readback_work_list[64][64];
-		readback_ssbo(readback_work_list, sizeof(readback_work_list), *work_list_buffer);
+		vec3 outputs[3];
+		readback_ssbo(outputs, sizeof(outputs), *outputs_buffer);
 
-		uint32_t readback_counts[64];
-		readback_ssbo(readback_counts, sizeof(readback_counts), *work_list_count);
+#define STEP(S) \
+		for (unsigned i = 0; i < S; i++) \
+		{ \
+			inputs[i][0] += inputs[i + S][0]; \
+			inputs[i][1] += inputs[i + S][1]; \
+			inputs[i][2] += inputs[i + S][2]; \
+		}
+		STEP(64);
+		STEP(32);
+		STEP(16);
+		STEP(8);
+		STEP(4);
+		STEP(2);
 
-		for (unsigned i = 0; i < 64; i++)
+		const vec3 reference[3] = {
+			inputs[0][2],
+			inputs[0][1],
+			inputs[1][0],
+		};
+
+		for (unsigned i = 0; i < 3; i++)
 		{
-			LOGI("Variant: %u\n", i);
-			LOGI("  Count: %u\n", readback_counts[i]);
-			for (unsigned j = 0; j < readback_counts[i]; j++)
-				LOGI("    %u\n", readback_work_list[i][j]);
+			LOGI("[%u] = (%f, %f, %f), expected (%f, %f, %f)\n", i,
+			     outputs[i][0], outputs[i][1], outputs[i][2],
+			     reference[i][0], reference[i][1], reference[i][2]);
+			bool is_equal = all(equal(outputs[i], reference[i]));
+			if (is_equal)
+				LOGI("All good\n");
+			else
+				LOGE(":(\n");
 		}
 
 		cmd = device.request_command_buffer();

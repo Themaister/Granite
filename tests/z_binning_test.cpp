4531175e42e3c5ac75586454ad40d2c2fc178713
Hans-Kristian Arntzen <maister@archlinux.us>
2021-08-11 14:07:54 +0200

    Compute bitranges in test.

--- tests/z_binning_test.cpp
+++ tests/z_binning_test.cpp
@@ -41,17 +41,34 @@ static int main_inner()
 	cmd->set_subgroup_size_log2(true, 5, 7);
 	cmd->enable_subgroup_size_control(true);
 
-	uvec4 inputs[128] = {};
-	std::mt19937 rnd(1234);
-	for (auto &inp : inputs)
-		for (int i = 0; i < 4; i++)
-			inp[i] = uint32_t(rnd());
+	constexpr unsigned NumInputs = 180;
+	constexpr unsigned NumRanges = 200;
 
-	auto input_buffer = create_ssbo(device, inputs, sizeof(inputs));
-	auto output_buffer = create_ssbo(device, nullptr, sizeof(inputs));
+	uvec2 inputs[NumInputs] = {};
+	for (auto &i : inputs)
+		i = uvec2(1000000000, 0);
+	inputs[40] = uvec2(189, 190);
+	inputs[170] = uvec2(190, 193);
+	inputs[171] = uvec2(191, 194);
+	inputs[172] = uvec2(192, 195);
+
+	auto input_buffer = create_ssbo(device, inputs, sizeof(uvec2) * NumInputs);
+	auto output_buffer = create_ssbo(device, nullptr, sizeof(uvec2) * NumRanges);
 	cmd->set_storage_buffer(0, 0, *input_buffer);
 	cmd->set_storage_buffer(0, 1, *output_buffer);
-	cmd->dispatch(1, 1, 1);
+
+	struct Push
+	{
+		uint32_t num_inputs;
+		uint32_t num_inputs_128;
+		uint32_t num_ranges;
+	} push = {};
+	push.num_inputs = NumInputs;
+	push.num_inputs_128 = (push.num_inputs + 127) / 128;
+	push.num_ranges = NumRanges;
+	cmd->push_constants(&push, 0, sizeof(push));
+
+	cmd->dispatch((NumRanges + 127) / 128, 1, 1);
 	cmd->barrier(VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT, VK_ACCESS_SHADER_WRITE_BIT,
 	             VK_PIPELINE_STAGE_HOST_BIT, VK_ACCESS_HOST_READ_BIT);
 
@@ -59,31 +76,13 @@ static int main_inner()
 	device.submit(cmd, &fence);
 	fence->wait();
 
-	auto *output_data = static_cast<const uvec4 *>(device.map_host_buffer(*output_buffer, MEMORY_ACCESS_READ_BIT));
-	for (uint32_t i = 0; i < 128; i++)
-		LOGI("Output %03u = 0x%08x%08x%08x%08x\n", i, output_data[i].w, output_data[i].z, output_data[i].y, output_data[i].x);
-	for (uint32_t i = 0; i < 128; i++)
-		LOGI("Input %03u = 0x%08x%08x%08x%08x\n", i, inputs[i].w, inputs[i].z, inputs[i].y, inputs[i].x);
-
-	const auto read_bit = [](const uvec4 *values, unsigned x, unsigned y) -> bool {
-		return (values[y][x / 32] & (1u << (x & 31))) != 0;
-	};
-
-	for (unsigned y = 0; y < 128; y++)
-	{
-		for (unsigned x = 0; x < 128; x++)
-		{
-			bool reference = read_bit(inputs, y, x);
-			bool actual = read_bit(output_data, x, y);
-			if (reference != actual)
-			{
-				LOGE("Mismatch at %u, %u.\n", x, y);
-				return EXIT_FAILURE;
-			}
-		}
-	}
-	LOGI("Success!\n");
+	auto *output_data = static_cast<const uvec2 *>(device.map_host_buffer(*output_buffer, MEMORY_ACCESS_READ_BIT));
+	for (uint32_t i = 0; i < NumRanges; i++)
+		LOGI("Output %03u = [%u, %u]\n", i, output_data[i].x, output_data[i].y);
+	for (uint32_t i = 0; i < NumInputs; i++)
+		LOGI("Input %03u = [%u, %u]\n", i, inputs[i].x, inputs[i].y);
 	device.unmap_host_buffer(*output_buffer, MEMORY_ACCESS_READ_BIT);
+
 	return EXIT_SUCCESS;
 }
 

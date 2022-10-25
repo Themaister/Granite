a926abfc3be77e5ac2cb6255a21d50cb4a7d5c42
Hans-Kristian Arntzen <post@themaister.net>
2022-10-25 14:07:26 +0200

    Add basic latency testing application.

---
+++ tests/latency_test.cpp
@@ -0,0 +1,165 @@
+/* Copyright (c) 2017-2022 Hans-Kristian Arntzen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "application.hpp"
+#include "command_buffer.hpp"
+#include "device.hpp"
+#include "os_filesystem.hpp"
+#include "font.hpp"
+#include "ui_manager.hpp"
+#include "string_helpers.hpp"
+#include "global_managers.hpp"
+#include "flat_renderer.hpp"
+#include "application_events.hpp"
+
+using namespace Granite;
+using namespace Vulkan;
+
+struct LatencyTest : Granite::Application, Granite::EventHandler
+{
+	LatencyTest()
+	{
+		EVENT_MANAGER_REGISTER(LatencyTest, on_key_down, KeyboardEvent);
+		frame_times.reserve(100);
+	}
+
+	bool on_key_down(const KeyboardEvent &e)
+	{
+		if (e.get_key_state() == KeyState::Pressed && e.get_key() == Key::Space)
+			state = !state;
+		return true;
+	}
+
+	void render_frame(double frame_time, double) override
+	{
+		auto &wsi = get_wsi();
+		auto &device = wsi.get_device();
+
+		if (frame_times.empty())
+			frame_times.resize(100, frame_time);
+		else
+		{
+			if (frame_times.size() >= 100)
+				frame_times.erase(frame_times.begin());
+			frame_times.push_back(frame_time);
+		}
+
+		double min_time = std::numeric_limits<double>::max();
+		double max_time = 0.0;
+		double avg_time = 0.0;
+		for (auto &t : frame_times)
+		{
+			min_time = std::min(min_time, t);
+			max_time = std::max(max_time, t);
+			avg_time += t;
+		}
+		avg_time /= double(frame_times.size());
+
+		auto cmd = device.request_command_buffer();
+		auto rp = device.get_swapchain_render_pass(SwapchainRenderPass::ColorOnly);
+
+		if (state)
+		{
+			rp.clear_color[0].float32[0] = 0.1f;
+			rp.clear_color[0].float32[1] = 0.2f;
+			rp.clear_color[0].float32[2] = 0.3f;
+		}
+		else
+		{
+			rp.clear_color[0].float32[0] = 0.3f;
+			rp.clear_color[0].float32[1] = 0.2f;
+			rp.clear_color[0].float32[2] = 0.1f;
+		}
+
+		cmd->begin_render_pass(rp);
+
+		flat.begin();
+		char avg_text[1024], min_text[1024], max_text[1024];
+		vec3 offset = { 10.0f, 10.0f, 0.0f };
+		vec2 size = { cmd->get_viewport().width - 20.0f, cmd->get_viewport().height - 20.0f };
+		snprintf(avg_text, sizeof(avg_text), "Average frame time: %.3f ms", 1000.0 * avg_time);
+		snprintf(min_text, sizeof(min_text), "Minimum frame time: %.3f ms", 1000.0 * min_time);
+		snprintf(max_text, sizeof(max_text), "Maximum frame time: %.3f ms", 1000.0 * max_time);
+		flat.render_text(GRANITE_UI_MANAGER()->get_font(UI::FontSize::Large),
+		                 avg_text, offset, size, vec4(1.0f, 1.0f, 0.0f, 1.0f),
+		                 Font::Alignment::TopRight, 1.0f);
+		offset.y += 30.0f;
+		flat.render_text(GRANITE_UI_MANAGER()->get_font(UI::FontSize::Normal),
+		                 min_text, offset, size, vec4(1.0f, 1.0f, 0.0f, 1.0f),
+		                 Font::Alignment::TopRight, 1.0f);
+		offset.y += 30.0f;
+		flat.render_text(GRANITE_UI_MANAGER()->get_font(UI::FontSize::Normal),
+		                 max_text, offset, size, vec4(1.0f, 1.0f, 0.0f, 1.0f),
+		                 Font::Alignment::TopRight, 1.0f);
+
+		offset = { cmd->get_viewport().width - 410.0f, cmd->get_viewport().height - 110.0f, 0.0f };
+		size = { 400.0f, 100.0f };
+		flat.render_quad(offset, size, vec4(0.0f, 0.0f, 0.0f, 0.9f));
+
+		vec2 offsets[100];
+
+		const auto remap_range = [&](double t) -> float {
+			if (t == min_time)
+				return 0.0f;
+			else if (t == max_time)
+				return 1.0f;
+			else
+				return float((t - min_time) / (max_time - min_time));
+		};
+
+		for (unsigned i = 0; i < 100; i++)
+		{
+			offsets[i].x = offset.x + float(i) / (100.0f - 1.0f) * size.x;
+			offsets[i].y = offset.y + size.y;
+			offsets[i].y -= remap_range(frame_times[i]) * size.y;
+		}
+		flat.render_line_strip(offsets, 0.0f, 100, vec4(1.0f, 1.0f, 0.0f, 1.0f));
+
+		flat.flush(*cmd, vec3(0.0f), { cmd->get_viewport().width, cmd->get_viewport().height, 1.0f });
+
+		cmd->end_render_pass();
+		device.submit(cmd);
+	}
+
+	std::vector<double> frame_times;
+	bool state = false;
+	FlatRenderer flat;
+};
+
+namespace Granite
+{
+Application *application_create(int, char **)
+{
+	GRANITE_APPLICATION_SETUP_FILESYSTEM();
+
+	try
+	{
+		auto *app = new LatencyTest();
+		return app;
+	}
+	catch (const std::exception &e)
+	{
+		LOGE("application_create() threw exception: %s\n", e.what());
+		return nullptr;
+	}
+}
+}

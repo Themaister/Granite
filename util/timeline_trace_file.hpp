1317c2e219f931682a64ca7cf30bb00b0b1c357f
Hans-Kristian Arntzen <post@themaister.net>
2023-10-30 21:50:42 +0100

    Add more safety when THREAD_GROUP does not exist.

--- util/timeline_trace_file.hpp
+++ util/timeline_trace_file.hpp
@@ -83,7 +83,7 @@ private:
 #define GRANITE_MACRO_CONCAT_IMPL(a, b) a##b
 #define GRANITE_MACRO_CONCAT(a, b) GRANITE_MACRO_CONCAT_IMPL(a, b)
 #define GRANITE_SCOPED_TIMELINE_EVENT(str) \
-	::Util::TimelineTraceFile::ScopedEvent GRANITE_MACRO_CONCAT(_timeline_scoped_count_, __COUNTER__){GRANITE_THREAD_GROUP()->get_timeline_trace_file(), str}
+	::Util::TimelineTraceFile::ScopedEvent GRANITE_MACRO_CONCAT(_timeline_scoped_count_, __COUNTER__){GRANITE_THREAD_GROUP() ? GRANITE_THREAD_GROUP()->get_timeline_trace_file() : nullptr, str}
 #define GRANITE_SCOPED_TIMELINE_EVENT_FILE(file, str) \
 	::Util::TimelineTraceFile::ScopedEvent GRANITE_MACRO_CONCAT(_timeline_scoped_count_, __COUNTER__){file, str}
 #else

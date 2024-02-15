d2da871a556b0c56dd22b21c16d6d3525db9c583
Hans-Kristian Arntzen <post@themaister.net>
2024-02-15 14:03:04 +0100

    Simple repro test.

--- tests/triangle_mesh.cpp
+++ tests/triangle_mesh.cpp
@@ -32,92 +32,28 @@ using namespace Vulkan;
 
 struct TriangleMeshApplication : Granite::Application, Granite::EventHandler
 {
-	BufferHandle create_readonly_ssbo(const void *data, size_t size)
-	{
-		BufferCreateInfo info = {};
-		info.usage = VK_BUFFER_USAGE_STORAGE_BUFFER_BIT;
-		info.domain = BufferDomain::LinkedDeviceHost;
-		info.size = size;
-		return get_wsi().get_device().create_buffer(info, data);
-	}
-
 	void render_frame(double, double elapsed_time)
 	{
 		auto &wsi = get_wsi();
 		auto &device = wsi.get_device();
-		auto &features = device.get_device_features();
-
-		if ((features.vk11_props.subgroupSupportedStages & VK_SHADER_STAGE_MESH_BIT_EXT) == 0)
-		{
-			LOGE("Subgroups not supported in mesh.\n");
-			return;
-		}
-
-		if ((features.vk11_props.subgroupSupportedStages & VK_SHADER_STAGE_TASK_BIT_EXT) == 0)
-		{
-			LOGE("Subgroups not supported in task.\n");
-			return;
-		}
-
-		if (!device.supports_subgroup_size_log2(true, 5, 5, VK_SHADER_STAGE_TASK_BIT_EXT))
-		{
-			LOGE("Wave32 not supported in task shader.\n");
-			return;
-		}
-
-		if (!device.supports_subgroup_size_log2(true, 5, 5, VK_SHADER_STAGE_MESH_BIT_EXT))
-		{
-			LOGE("Wave32 not supported in mesh shader.\n");
-			return;
-		}
 
 		auto cmd = device.request_command_buffer();
 
 		cmd->begin_render_pass(device.get_swapchain_render_pass(SwapchainRenderPass::ColorOnly));
-		cmd->set_program("assets://shaders/triangle.task",
+		cmd->set_program("",
 		                 "assets://shaders/triangle.mesh",
 		                 "assets://shaders/triangle_mesh.frag");
 		cmd->set_opaque_state();
 
-		cmd->enable_subgroup_size_control(true, VK_SHADER_STAGE_TASK_BIT_EXT);
-		cmd->enable_subgroup_size_control(true, VK_SHADER_STAGE_MESH_BIT_EXT);
-		cmd->set_subgroup_size_log2(true, 5, 5, VK_SHADER_STAGE_TASK_BIT_EXT);
-		cmd->set_subgroup_size_log2(true, 5, 5, VK_SHADER_STAGE_MESH_BIT_EXT);
+		uint32_t indirect_data[] = { 3, 2, 1 };
 
-		vec2 vertices[] = {
-			vec2(-0.2f, -0.2f),
-			vec2(-0.2f, +0.2f),
-			vec2(+0.2f, -0.2f),
-		};
+		BufferCreateInfo info = {};
+		info.size = 12;
+		info.usage = VK_BUFFER_USAGE_INDIRECT_BUFFER_BIT;
+		info.domain = BufferDomain::LinkedDeviceHost;
+		auto indirect_buf = device.create_buffer(info, indirect_data);
 
-		auto c = float(muglm::cos(elapsed_time * 2.0));
-		auto s = float(muglm::sin(elapsed_time * 2.0));
-		mat2 m{vec2(c, -s), vec2(s, c)};
-		for (auto &v : vertices)
-			v = m * v;
-
-		static const vec4 colors[] = {
-			vec4(1.0f, 0.0f, 0.0f, 1.0f),
-			vec4(0.0f, 1.0f, 0.0f, 1.0f),
-			vec4(0.0f, 0.0f, 1.0f, 1.0f),
-			vec4(1.0f, 1.0f, 1.0f, 1.0f),
-		};
-
-		static const vec2 offsets[] = {
-			vec2(-0.5f, -0.5f),
-			vec2(+0.5f, -0.5f),
-			vec2(-0.5f, +0.5f),
-			vec2(+0.5f, +0.5f),
-		};
-
-		auto pos = create_readonly_ssbo(vertices, sizeof(vertices));
-		cmd->set_storage_buffer(0, 0, *pos);
-		auto offbuf = create_readonly_ssbo(offsets, sizeof(offsets));
-		cmd->set_storage_buffer(0, 1, *offbuf);
-		auto colbuf = create_readonly_ssbo(colors, sizeof(colors));
-		cmd->set_storage_buffer(0, 2, *colbuf);
-
-		cmd->draw_mesh_tasks(1, 1, 1);
+		cmd->draw_mesh_tasks_indirect(*indirect_buf, 0, 1, 12);
 		cmd->end_render_pass();
 		device.submit(cmd);
 	}

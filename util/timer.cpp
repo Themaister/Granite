87ba366eace49e2d7f82755d14f01e6b4b58a4bd
Stefan Schlosser <bsdcode@disroot.org>
2024-07-21 22:39:17 +0200

    Use CLOCK_MONOTONIC on FreeBSD
    FreeBSD doesn't have CLOCK_MONOTONIC_RAW.

--- util/timer.cpp
+++ util/timer.cpp
@@ -107,7 +107,7 @@ int64_t get_current_time_nsecs()
 	return int64_t(double(li.QuadPart) * static_qpc_freq.inv_freq);
 #else
 	struct timespec ts = {};
-#ifdef ANDROID
+#if defined(ANDROID) || defined(__FreeBSD__)
 	constexpr auto timebase = CLOCK_MONOTONIC;
 #else
 	constexpr auto timebase = CLOCK_MONOTONIC_RAW;

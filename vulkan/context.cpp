ddfeddce27bb23f98a6ded736b68fba73d5cb003
Hans-Kristian Arntzen <post@themaister.net>
2025-08-24 13:46:12 +0200

    Disable descriptor buffers in debug builds.
    For VVL purposes mostly since we need full GPU VA.

--- vulkan/context.cpp
+++ vulkan/context.cpp
@@ -1543,12 +1543,14 @@ bool Context::create_device(VkPhysicalDevice gpu_, VkSurfaceKHR surface,
 		ADD_CHAIN(ext.robustness2_features, ROBUSTNESS_2_FEATURES_EXT);
 	}
 
+#ifndef VULKAN_DEBUG
 	if ((flags & CONTEXT_CREATION_ENABLE_DESCRIPTOR_BUFFER_BIT) != 0 &&
 	    has_extension(VK_EXT_DESCRIPTOR_BUFFER_EXTENSION_NAME))
 	{
 		ADD_CHAIN(ext.descriptor_buffer_features, DESCRIPTOR_BUFFER_FEATURES_EXT);
 		enabled_extensions.push_back(VK_EXT_DESCRIPTOR_BUFFER_EXTENSION_NAME);
 	}
+#endif
 
 	if ((flags & CONTEXT_CREATION_ENABLE_ADVANCED_WSI_BIT) != 0 && requires_swapchain)
 	{
